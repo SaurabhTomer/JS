682e4a021b31000c64de103d744ed5d6af446892
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-08-30 10:26:29 +0530

    fisrt commit

---
+++ 02_basics/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
+
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
---
+++ Basics/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
