da2c0e277f4e72823a83b57aa7f8dbfec9214c1d
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-08-30 11:48:53 +0530

    Clock Project

---
+++ Projects/Project3/index.js
@@ -0,0 +1,13 @@
+const clock = document.getElementById('clock');
+
+// const clock = document.querySelector('clock');
+
+
+
+setInterval(function () {
+    let date = new Date();
+    clock.innerText = date.toLocaleTimeString();
+    // console.log(date.toLocaleTimeString());
+}, 1000)
+
+
